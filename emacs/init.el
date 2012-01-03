3a18875927fa44cf4104a0432bc80e1c1c4b2235
tkzwtks <tkzwtks@gmail.com>
2012-01-04 00:41:38 +0900

    emacs configure

---
+++ emacs/init.el
@@ -0,0 +1,104 @@
+;;; ロードパスの追加
+(setq load-path (append
+                 '("~/.emacs.d"
+                   "~/.emacs.d/packages"
+                   "~/.emacs.d/packages/hand-install"
+                   "~/.emacs.d/packages/hand-install/scala"
+                   "~/.emacs.d/auto-install")
+                 load-path))
+
+;; フォントセットを作る
+(let* ((fontset-name "myfonts") ; フォントセットの名前
+       (size 12) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
+       (asciifont "monaco") ; ASCIIフォント
+       (jpfont "Hiragino Maru Gothic ProN") ; 日本語フォント
+       (font (format "%s-%d:weight=normal:slant=normal" asciifont size))
+       (fontspec (font-spec :family asciifont))
+       (jp-fontspec (font-spec :family jpfont)) 
+       (fsn (create-fontset-from-ascii-font font nil fontset-name)))
+  (set-fontset-font fsn 'japanese-jisx0213.2004-1 jp-fontspec)
+  (set-fontset-font fsn 'japanese-jisx0213-2 jp-fontspec)
+  (set-fontset-font fsn 'katakana-jisx0201 jp-fontspec) ; 半角カナ
+  (set-fontset-font fsn '(#x0080 . #x024F) fontspec) ; 分音符付きラテン
+  (set-fontset-font fsn '(#x0370 . #x03FF) fontspec) ; ギリシャ文字
+  )
+
+(add-to-list 'default-frame-alist '(font . "fontset-myfonts"))
+
+;; カーソルの点滅を止める
+(blink-cursor-mode 0)
+
+;; 行末の空白表示
+;;(setq-default show -trailing-whitespace t)
+
+;;; 現在行を目立たせる
+(global-hl-line-mode)
+
+;;; カーソルの位置が何文字目かを表示する
+(column-number-mode t)
+
+;;; カーソルの位置が何行目かを表示する
+(line-number-mode t)
+
+;;; カーソルの場所を保存する
+(require 'saveplace)
+(setq-default save-place t)
+
+;;; バックアップファイルを作らない
+(setq backup-inhibited t)
+
+;;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+
+;;; 補完時に大文字小文字を区別しない
+(setq completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+
+;;; 部分一致の補完機能を使う
+;;; p-bでprint-bufferとか
+(partial-completion-mode t)
+
+;;; 補完可能なものを随時表示
+;;; 少しうるさい
+(icomplete-mode 1)
+
+;;; diredを便利にする
+(require 'dired-x)
+
+;;; diredから"r"でファイル名をインライン編集する
+(require 'wdired)
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+
+;;; ファイル名が重複していたらディレクトリ名を追加する。
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+;;; 標準Elispの設定
+(load "config/builtins")
+
+;;; 非標準Elispの設定
+(load "config/packages")
+
+;;; 見た目の設定
+(load "config/faces")
+
+;;; anythig-mode
+(require 'anything-startup)
+
+;;; exec-path
+;; より下に記述した物が PATH の先頭に追加されます
+(dolist (dir (list
+              "/sbin"
+              "/usr/sbin"
+              "/bin"
+              "/usr/bin"
+              "/opt/local/bin"
+              "/sw/bin"
+              "/usr/local/bin"
+              (expand-file-name "~/bin")
+              (expand-file-name "~/.emacs.d/bin")
+              ))
+ ;; PATH と exec-path に同じ物を追加します
+ (when (and (file-exists-p dir) (not (member dir exec-path)))
+   (setenv "PATH" (concat dir ":" (getenv "PATH")))
+   (setq exec-path (append (list dir) exec-path))))
