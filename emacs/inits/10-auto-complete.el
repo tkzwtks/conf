0feb6311bacc2da9d4b801334743c7ed1421071f
Takashi Takizawa <tkzwtks@gmail.com>
2016-06-09 01:53:49 +0900

    色々追加

---
+++ emacs/inits/10-auto-complete.el
@@ -0,0 +1,6 @@
+(el-get-bundle auto-complete)
+(el-get-bundle fuzzy)
+(ac-config-default)
+
+(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-fuzzy t)          ;; 曖昧マッチ
---
+++ emacs/inits/10-perl.el
@@ -0,0 +1,20 @@
+(defalias 'perl-mode 'cperl-mode)
+(el-get-bundle perl-completion)
+
+(add-hook 'cperl-mode-hook
+	  (lambda ()
+	    (when (require 'auto-complete nil t) ; no error whatever auto-complete.el is not installed.
+	      (auto-complete-mode t)
+	      (make-variable-buffer-local 'ac-sources)
+	      (setq ac-sources
+		    '(ac-source-perl-completion)))))
+
+;; インデント
+(setq cperl-indent-level 4 ; インデント幅
+      cperl-continued-statement-offset 4 ; 継続する文のオフセット
+      cperl-brace-offset -4 ; ブレースのオフセット
+      cperl-label-offset -4 ; labelのオフセット
+      cperl-indent-parens-as-block t ; 括弧もブロックとしてインデント
+      cperl-close-paren-offset -4 ; 閉じ括弧のオフセット
+      cperl-tab-always-indent t ; TABをインデントにする
+      cperl-highlight-variables-indiscriminately t) ; スカラを常にハイライト
