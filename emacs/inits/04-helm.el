ce215f18429ddd674dffb7b79d2c9f9f64c0c051
Takashi Takizawa <tkzwtks@gmail.com>
2016-06-09 01:53:24 +0900

    find-fileは標準のモノを使う

--- emacs/inits/04-helm.el
+++ emacs/inits/04-helm.el
@@ -12,6 +12,5 @@
                                )))
 
 (define-key global-map (kbd "C-x b") 'helm-mini)
-(define-key global-map (kbd "C-x C-f") 'helm-find-files)
 (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
 (define-key global-map (kbd "M-x") 'helm-M-x)
