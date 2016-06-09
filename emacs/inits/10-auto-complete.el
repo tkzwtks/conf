12ebe5e2ed1b3b28a47eede69220ba76974f169b
Takashi Takizawa <tkzwtks@gmail.com>
2016-06-10 00:18:54 +0900

    delay値とか調整

--- emacs/inits/10-auto-complete.el
+++ emacs/inits/10-auto-complete.el
@@ -1,6 +1,11 @@
 (el-get-bundle auto-complete)
 (el-get-bundle fuzzy)
-(ac-config-default)
 
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-dictionary-directories (locate-user-emacs-file "dict"))
+(setq ac-delay 0.1)
+(setq ac-auto-show-menu 0.2)
 (setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
 (setq ac-use-fuzzy t)          ;; 曖昧マッチ
+
