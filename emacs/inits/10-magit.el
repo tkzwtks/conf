1126d797a4c9d9f75b5fe90e02a46500fa0463c4
Takashi Takizawa <tkzwtks@gmail.com>
2016-06-10 01:23:26 +0900

    ウインドウ構成を維持するように修正

--- emacs/inits/10-magit.el
+++ emacs/inits/10-magit.el
@@ -1,2 +1,19 @@
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
+
+;; window構成維持する
+;; see http://syohex.hatenablog.com/entry/20130904/1378310201
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun my/magit-quit-session ()
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
+
+(defadvice git-commit-commit (after move-to-magit-buffer activate)
+  (delete-window))
