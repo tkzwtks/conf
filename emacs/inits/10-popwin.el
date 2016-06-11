81845772822a688080200b404dfcb112a7b5f51c
Takashi Takizawa <tkzwtks@gmail.com>
2016-06-11 13:37:46 +0900

    helmでpopwin使わない

--- emacs/inits/10-popwin.el
+++ emacs/inits/10-popwin.el
@@ -1,5 +1,3 @@
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
 (setq popwin:popup-window-height 0.3)
-
-(push '("helm" :regexp t :height 0.3) popwin:special-display-config)
