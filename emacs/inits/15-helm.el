898e8dc226cec11d9204a8dc256eb7f0debe2582
Takashi Takizawa <tkzwtks@gmail.com>
2016-06-11 13:48:37 +0900

    やっぱりhelm-find-file使う

--- emacs/inits/15-helm.el
+++ emacs/inits/15-helm.el
@@ -5,11 +5,15 @@
 (custom-set-variables
  '(helm-mini-default-sources '(helm-source-buffers-list
                                helm-source-recentf
-			       helm-source-ls-git
                                helm-source-files-in-current-dir
                                )))
 
 (setq helm-samewindow nil)
 (define-key global-map (kbd "C-x b") 'helm-mini)
+
+;; helm-find-files使う。TABで補完する。
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+
 (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
 (define-key global-map (kbd "M-x") 'helm-M-x)
