aef711b99f96e0c4cbc94b55710bbc96fce72424
tkzwtks <tkzwtks@hatena.ne.jp>
2016-07-01 21:47:41 +0900

    project_libの設定ちょっといじる

--- emacs/inits/10-flycheck.el
+++ emacs/inits/10-flycheck.el
@@ -1,4 +1,22 @@
 (require 'flycheck)
 (setq flycheck-check-syntax-automatically '(mode-enabled new-line))
 
-(add-hook 'cperl-mode-hook 'flycheck-mode)
+;; for perl
+;; see http://m0t0k1ch1st0ry.com/blog/2014/07/07/flycheck/
+(flycheck-define-checker perl-project-libs
+  "A perl syntax checker."
+  :command ("perl"
+            "-MProject::Lib lib_dirs => [qw(local/lib/perl5), glob(qw(modules/*/lib)), glob(qw(t/*/lib))]"
+            "-wc"
+            source-inplace)
+  :error-patterns ((error line-start
+                          (minimal-match (message))
+                          " at " (file-name) " line " line
+                          (or "." (and ", " (zero-or-more not-newline)))
+                          line-end))
+  :modes (cperl-mode))
+
+(add-hook 'cperl-mode-hook
+          (lambda ()
+            (flycheck-mode t)
+            (setq flycheck-checker 'perl-project-libs)))
