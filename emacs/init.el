65d044b652eaee8594394b4d0d218bdd5c8352d7
tkzwtks <tkzwtks@hatena.ne.jp>
2017-06-11 09:52:29 +0900

    helmの調整

--- emacs/init.el
+++ emacs/init.el
@@ -31,3 +31,20 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(git-gutter:update-interval 2)
+ '(helm-mini-default-sources
+   (quote
+    (helm-source-buffers-list helm-source-recentf helm-source-files-in-current-dir)))
+ '(package-selected-packages (quote (nil let-alist))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
